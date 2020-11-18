c40d1df4a3433ef18c16b56e69716c5a9cdda434
Camilo Villalobos <camilo.svs@gmail.com>
2020-11-18 13:06:19 -0300

    add archivos

---
+++ application/views/about.php
@@ -0,0 +1,29 @@
+<!-- About Section-->
+<section class="page-section bg-primary text-white mb-0" id="about">
+	<div class="container">
+		<!-- About Section Heading-->
+		<h2 class="page-section-heading text-center text-uppercase text-white">Sobre Mí</h2>
+		<!-- Icon Divider-->
+		<div class="divider-custom divider-light">
+			<div class="divider-custom-line"></div>
+			<div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+			<div class="divider-custom-line"></div>
+		</div>
+		<!-- About Section Content-->
+		<div class="row">
+			<div class="col-lg-4 ml-auto"><p class="lead"style="text-align: justify">Estudiante de la Universida de Talca, actualmetne en cuarto
+					año de enseñanza y actual ayudante de Programación Avanzada sobre el Paradigma Orientado a
+					objetos.Mis pasatiempos favoritos, aparte de los videojuegos y mis
+					mascotas, es aprender sobre ciberseguridad y pentesting. Esta área me apasiona mucho y me
+					gustaría dedicarme a ella en un futuro cercano.</p></div>
+
+			<div class="col-lg-4 mr-auto"><p class="lead" style="text-align: justify">Poseo ferreos conocimientos en programación, destacándose los
+					lenguajes de Java,
+					Python y C#. Domino de herramientas como NetBeans, Intellij IDEA, PHP Storm, Visual Studio Code, Spyder. En el ámbito de Redes y
+					ciberseguridad poseo buenos conocimientos de Cisco, Administración de Redes, Virtualización, Dockerizacion y dominio de Linux
+					nivel
+					Intermedio.
+				</p></div>
+		</div>
+	</div>
+</section>
---
+++ application/views/modal.php
@@ -0,0 +1,126 @@
+<!-- Portfolio Modals-->
+<!-- Portfolio Modal 1-->
+<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-labelledby="portfolioModal1Label" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true"><i class="fas fa-times"></i></span>
+			</button>
+			<div class="modal-body text-center">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-8">
+							<!-- Portfolio Modal - Title-->
+							<h2 class="portfolio-modal-title text-secondary text-uppercase mb-0" id="portfolioModal1Label">GitHub</h2>
+							<!-- Icon Divider-->
+							<div class="divider-custom">
+								<div class="divider-custom-line"></div>
+								<div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+								<div class="divider-custom-line"></div>
+							</div>
+							<!-- Portfolio Modal - Image-->
+							<img class="img-fluid rounded mb-5" src="assets/img/portfolio/git.png" alt="" />
+							<!-- Portfolio Modal - Text-->
+							<p class="mb-5">Muchos de los proyectos realizados para diversas asignaturas estan en mi GitHub. Hay varias
+								interesantes cosas que ver, como un juego tipo bubble shooter desarrollado en python y pygame, Algoritmos de
+								Machine Learning, Proyectos de uso de Threads en C, Algoritmos para operar con Lenguajes Regulares, entre otros. Te
+								invito a hacer click en el boton de GitHub para dirigirse a mis repositorios. </p>
+							<button class="btn btn-info" onclick="window.location='https://github.com/Camilovs'">
+								<i class="fab fa-github fa-fw"></i>
+								GitHub
+							</button>
+							<button class="btn btn-primary" data-dismiss="modal">
+								<i class="fas fa-times fa-fw"></i>
+								Close Window
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Portfolio Modal 2-->
+<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-labelledby="portfolioModal2Label" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true"><i class="fas fa-times"></i></span>
+			</button>
+			<div class="modal-body text-center">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-8">
+							<!-- Portfolio Modal - Title-->
+							<h2 class="portfolio-modal-title text-secondary text-uppercase mb-0" id="portfolioModal2Label">COBALL</h2>
+							<!-- Icon Divider-->
+							<div class="divider-custom">
+								<div class="divider-custom-line"></div>
+								<div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+								<div class="divider-custom-line"></div>
+							</div>
+							<!-- Portfolio Modal - Image-->
+							<img class="img-fluid rounded mb-5" src="assets/img/portfolio/bubblegame.png" alt="" />
+							<!-- Portfolio Modal - Text-->
+							<p class="mb-5">COBALL es un juego desarrollado en python con la biblioteca pygame. Este juego es un bubble game donde
+								el objetivo principal es reventar las burbujas del mismo color, hasta no tener ninguna en el tablero y es cuando se
+								gana. Desarrollado para el curso de Solucion Algoritmica de Problemas. Haga click en el boton de Github para
+								dirigirse al repositorio del proyecto.
+							</p>
+							<button class="btn btn-info" onclick="window.location='https://github.com/Camilovs/Proyecto_Bubble'">
+								<i class="fab fa-github fa-fw"></i>
+								GitHub
+							</button>
+							<button class="btn btn-primary" data-dismiss="modal">
+								<i class="fas fa-times fa-fw"></i>
+								Close Window
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Portfolio Modal 3-->
+<div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-labelledby="portfolioModal3Label" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true"><i class="fas fa-times"></i></span>
+			</button>
+			<div class="modal-body text-center">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-lg-8">
+							<!-- Portfolio Modal - Title-->
+							<h2 class="portfolio-modal-title text-secondary text-uppercase mb-0" id="portfolioModal3Label">Tetris de Imagenes</h2>
+							<!-- Icon Divider-->
+							<div class="divider-custom">
+								<div class="divider-custom-line"></div>
+								<div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+								<div class="divider-custom-line"></div>
+							</div>
+							<!-- Portfolio Modal - Image-->
+							<img class="img-fluid rounded mb-5" src="assets/img/portfolio/tetris.png" alt="" />
+							<!-- Portfolio Modal - Text-->
+							<p class="mb-5">Tetris de imágenes es un proyecto desarrollado para la Asignatura de Programación avanzada, de la cual
+								ahora soy ayudante. Este proyecto trata de un juego tetris pero utilizando una imagen, de forma que el objetivo del
+								juego sea reconstruir la imagen original, colocando las piezas de manera correcta. Esta desarrollado 100% en Java.
+								Haga click enel botón de Github para dirigirse al repositorio del proyecto.</p>
+							<button class="btn btn-info" onclick="window.location='https://github.com/Camilovs/Proyecto_tetrisImage'">
+								<i class="fab fa-github fa-fw"></i>
+								GitHub
+							</button>
+							<button class="btn btn-primary" data-dismiss="modal">
+								<i class="fas fa-times fa-fw"></i>
+								Close Window
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
